409f183470331c79121db879606100b15b9a2008
Miguel Cazares <miguel.cazares89@gmail.com>
2026-04-06 18:24:23 -0500

    build: prep nearby director sync for ios build 151

--- src/offlineWebBundle.js
+++ src/offlineWebBundle.js
@@ -1,4 +1,4 @@
-const OFFLINE_WEB_BUNDLE_VERSION = "150";
+const OFFLINE_WEB_BUNDLE_VERSION = "151";
 
 const OFFLINE_WEB_BUNDLE_ASSETS = {
   "app.bundle": require("../assets/offline-web/app.bundle"),
