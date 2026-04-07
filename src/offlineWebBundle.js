89ef3812df8cdb2450320e884e74af5fd674a4dc
Miguel Cazares <miguel.cazares89@gmail.com>
2026-04-07 15:18:13 -0500

    build: bump ios build number to 153

--- src/offlineWebBundle.js
+++ src/offlineWebBundle.js
@@ -1,4 +1,4 @@
-const OFFLINE_WEB_BUNDLE_VERSION = "152";
+const OFFLINE_WEB_BUNDLE_VERSION = "153";
 
 const OFFLINE_WEB_BUNDLE_ASSETS = {
   "app.bundle": require("../assets/offline-web/app.bundle"),
