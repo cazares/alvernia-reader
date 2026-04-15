f4fa6e370309f9d61cabaeb8727ca7ef71dafd41
Miguel Cazares <miguel.cazares89@gmail.com>
2026-04-15 16:00:44 -0500

    build: bump app to 220 for TestFlight

--- src/offlineWebBundle.js
+++ src/offlineWebBundle.js
@@ -1,4 +1,4 @@
-const OFFLINE_WEB_BUNDLE_VERSION = "219";
+const OFFLINE_WEB_BUNDLE_VERSION = "220";
 
 const OFFLINE_WEB_BUNDLE_ASSETS = {
   "app.bundle": require("../assets/offline-web/app.bundle"),
