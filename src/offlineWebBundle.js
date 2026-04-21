1a9b246e823e70a52fdeaa4c308dd13b7bf68450
Miguel Cazares <miguel.cazares89@gmail.com>
2026-04-20 22:25:07 -0500

    Make EXConstants app.config script deterministic

--- src/offlineWebBundle.js
+++ src/offlineWebBundle.js
@@ -1,4 +1,4 @@
-const OFFLINE_WEB_BUNDLE_VERSION = "234";
+const OFFLINE_WEB_BUNDLE_VERSION = "235";
 
 const OFFLINE_WEB_BUNDLE_ASSETS = {
   "app.bundle": require("../assets/offline-web/app.bundle"),
