e8f155e0304b0b92bfd3e2f531d080c7a459e886
Miguel Cazares <miguel.cazares89@gmail.com>
2026-04-21 12:17:28 -0500

    Bump build to 238

--- src/offlineWebBundle.js
+++ src/offlineWebBundle.js
@@ -1,4 +1,4 @@
-const OFFLINE_WEB_BUNDLE_VERSION = "237";
+const OFFLINE_WEB_BUNDLE_VERSION = "238";
 
 const OFFLINE_WEB_BUNDLE_ASSETS = {
   "app.bundle": require("../assets/offline-web/app.bundle"),
